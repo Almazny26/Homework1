57a857b9b2fec4c5f8539ead9b5a26ea9d6af4f5
Almazny26 <karabanovdmitrijj@gmail.com>
2024-02-14 15:32:39 +0300

    completed task 4

--- skriptHW-2.js
+++ skriptHW-2.js
@@ -13,3 +13,8 @@ alert(`Первый iPhone вышел в ${yearFirstIPhone} году.`);
 // 3 task
 const name = "Брендан Эйх";
 alert(`Создателя JavaScript зовут ${name}.`);
+
+// 4 task
+let x = 10;
+let y = 2;
+alert(`Сумма: ${x + y}, Разность: ${x - y}, Произведение: ${x * y}, Частное: ${x / 2}`);
