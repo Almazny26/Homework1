2b04ff22c483e61ebff85c9a01076b042cedf5f3
Dmitry Karabanov <karabanovdmitrijj@gmail.com>
2024-03-11 22:33:00 +0300

    2 the task is done

--- JS/hw-7.js
+++ JS/hw-7.js
@@ -7,3 +7,29 @@ console.log(text.toUpperCase());
 
 
 
+// 2 task
+
+function filterStringsByPrefix(stringArray, prefix) {
+    prefix = prefix.toLowerCase();
+    let filteredArray = [];
+    for (let i = 0; i < stringArray.length; i++) {
+        let lowerString = stringArray[i].toLowerCase();
+
+        if (lowerString.startsWith(prefix)) {
+            filteredArray.push(stringArray[i]);
+        }
+    }
+    return filteredArray;
+}
+
+let arr = ["кошка", "собака", "Корова", "Николай", "машина"];
+let prefix = "ко";
+let result = filterStringsByPrefix(arr, prefix);
+console.log(result);
+
+
+
+
+
+// 3 task
+
