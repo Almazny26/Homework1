7d17aaace80f581c2da7e03f08f554f9d25280ab
Dmitry Karabanov <karabanovdmitrijj@gmail.com>
2024-03-11 23:13:44 +0300

    completed task 3 and 4

--- JS/hw-7.js
+++ JS/hw-7.js
@@ -33,3 +33,30 @@ console.log(result);
 
 // 3 task
 
+let number = 32.58884;
+let roundedDown = Math.floor(number);
+let roundedUp = Math.ceil(number);
+let roundedNearest = Math.round(number);
+
+console.log('До меньшего целого:', roundedDown);
+console.log('До большего целого:', roundedUp);
+console.log('До ближайшего целого:', roundedNearest);
+
+
+
+
+
+// 4 task
+
+let numMaxMin = [52, 53, 49, 77, 21, 32];
+let max = Math.max(...numMaxMin);
+let min = Math.min(...numMaxMin);
+
+console.log('Максимальное значение:', max);
+console.log('Минимальное значение:', min);
+
+
+
+
+
+// 5 task
