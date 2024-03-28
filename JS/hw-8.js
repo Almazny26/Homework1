4f8e1c863722e3a5919b7a18a364840586f73fe7
Dmitry Karabanov <karabanovdmitrijj@gmail.com>
2024-03-28 23:51:11 +0300

    completed 2 tasks

--- JS/hw-8.js
+++ JS/hw-8.js
@@ -1,14 +1,50 @@
-// 1 task
+// // 1 task
 
-const people = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 }
-];
+// const people = [
+//     { name: 'Глеб', age: 29 },
+//     { name: 'Анна', age: 17 },
+//     { name: 'Олег', age: 7 },
+//     { name: 'Оксана', age: 47 }
+// ];
 
-function agePeople(people1, people2) {
-    return people1.age - people2.age;
+// function agePeople(people1, people2) {
+//     return people1.age - people2.age;
+// }
+
+// console.log(people.sort(agePeople));
+
+
+
+
+// 2 task
+
+function isPositive(num) {
+    return num > 0;
+
+}
+function isMale(person) {
+    return person.gender === 'male';
 }
 
-console.log(people.sort(agePeople));
+function filter(arr, ruleFunction) {
+    const filteredArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (ruleFunction(arr[i])) {
+            filteredArray.push(arr[i]);
+        }
+    }
+
+    return filteredArray;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const people = [
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
+];
+
+console.log(filter(people, isMale));
