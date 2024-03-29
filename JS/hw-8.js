58f967efb59e1b91d18bce2e93b988508dd10a71
Dmitry Karabanov <karabanovdmitrijj@gmail.com>
2024-03-29 12:05:16 +0300

    completed 3 tasks

--- JS/hw-8.js
+++ JS/hw-8.js
@@ -16,35 +16,53 @@
 
 
 
-// 2 task
+// // 2 task
 
-function isPositive(num) {
-    return num > 0;
+// function isPositive(num) {
+//     return num > 0;
 
+// }
+// function isMale(person) {
+//     return person.gender === 'male';
+// }
+
+// function filter(arr, ruleFunction) {
+//     const filteredArray = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (ruleFunction(arr[i])) {
+//             filteredArray.push(arr[i]);
+//         }
+//     }
+
+//     return filteredArray;
+// }
+
+// console.log(filter([3, -4, 1, 9], isPositive));
+
+// const people = [
+//     { name: 'Глеб', gender: 'male' },
+//     { name: 'Анна', gender: 'female' },
+//     { name: 'Олег', gender: 'male' },
+//     { name: 'Оксана', gender: 'female' }
+// ];
+
+// console.log(filter(people, isMale));
+
+
+
+
+
+// 3 task
+
+function printCurrentDate() {
+    const currentDate = new Date();
+    console.log(currentDate);
 }
-function isMale(person) {
-    return person.gender === 'male';
-}
 
-function filter(arr, ruleFunction) {
-    const filteredArray = [];
+const intervalId = setInterval(printCurrentDate, 3000);
 
-    for (let i = 0; i < arr.length; i++) {
-        if (ruleFunction(arr[i])) {
-            filteredArray.push(arr[i]);
-        }
-    }
-
-    return filteredArray;
-}
-
-console.log(filter([3, -4, 1, 9], isPositive));
-
-const people = [
-    { name: 'Глеб', gender: 'male' },
-    { name: 'Анна', gender: 'female' },
-    { name: 'Олег', gender: 'male' },
-    { name: 'Оксана', gender: 'female' }
-];
-
-console.log(filter(people, isMale));
+setTimeout(() => {
+    clearInterval(intervalId);
+    console.log("30 секунд прошло");
+}, 30000);
