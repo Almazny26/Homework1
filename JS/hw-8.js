d92fb82f99339d6dfe041a4136ef91cb48c26e74
Dmitry Karabanov <karabanovdmitrijj@gmail.com>
2024-03-29 21:02:25 +0300

    deleted the previous branch and created a new one, overwritten the files

---
+++ JS/hw-8.js
@@ -0,0 +1,101 @@
+// 1 task
+
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+];
+
+function agePeople(people1, people2) {
+    return people1.age - people2.age;
+}
+
+console.log(people.sort(agePeople));
+
+
+
+
+// 2 task
+
+function isPositive(num) {
+    return num > 0;
+
+}
+function isMale(person) {
+    return person.gender === 'male';
+}
+
+function filter(arr, ruleFunction) {
+    const filteredArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (ruleFunction(arr[i])) {
+            filteredArray.push(arr[i]);
+        }
+    }
+
+    return filteredArray;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const people = [
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
+];
+
+console.log(filter(people, isMale));
+
+
+
+
+
+// 3 task
+
+function printCurrentDate() {
+    const currentDate = new Date();
+    console.log(currentDate);
+}
+
+const intervalId = setInterval(printCurrentDate, 3000);
+
+setTimeout(() => {
+    clearInterval(intervalId);
+    console.log("30 секунд прошло");
+}, 30000);
+
+
+
+
+
+// 4 task
+
+function delayForSecond(callback) {
+    setTimeout(callback, 1000);
+}
+
+delayForSecond(function () {
+    console.log('Привет, Глеб!');
+})
+
+
+
+
+
+// 5 task
+
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if (cb) { cb(); }
+    }, 1000)
+}
+
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+}
+
+delayForSecond(() => sayHi('Глеб'));
